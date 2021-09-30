abb55eb7e2b6abecea1410959245d53e4bfdb85f
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-09-30 22:03:22 +0300

    delete tag module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [TagsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
