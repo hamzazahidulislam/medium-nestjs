6a3ab0bf4be11efb06b2e284a7b999742a357993
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-01 03:32:37 +0300

    app module updated and add ormconfig and migrate the table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,46 +2,11 @@ import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as Joi from 'joi';
+import ormconfig from './ormconfig';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      // envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      // ignoreEnvFile: process.env.NODE_ENV === 'production',
-      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev',
-      ignoreEnvFile: process.env.NODE_ENV === 'test',
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
-        DB_HOST: Joi.string(),
-        DB_PORT: Joi.string(),
-        DB_USERNAME: Joi.string(),
-        DB_PASSWORD: Joi.string(),
-        DB_NAME: Joi.string(),
-      }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      ...(process.env.DATABASE_URL
-        ? { url: process.env.DATABASE_URL }
-        : {
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-          }),
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging:
-        process.env.NODE_ENV !== 'production' &&
-        process.env.NODE_ENV !== 'test',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
-    TagModule,
-  ],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'user',
+  password: 'password',
+  database: 'db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
