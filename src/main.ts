62aed9c37398726000e8f1db8f61b11cf32070cf
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-03 05:20:48 +0300

    swagger set-up complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ if (!process.env.IT_TS_NODE) {
 }
 import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Medium NestJS')
+    .setDescription('Medium API')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
