bbd2e01caccd2b1c35953e46d8266fbec1e370db
Hamza Zahiul Islam <hamzazahidulislam444@gmail.com>
2021-10-08 13:19:11 +0300

    create a profile module and profile controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { ArticleModule } from './article/article.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
