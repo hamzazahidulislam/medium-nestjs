810d119b63a429d96f4c42e5c98f2d145f7927ac
Hamza Zahiul Islam <hamzazahidulislam444@gmail.com>
2021-10-08 21:55:57 +0300

    add followProfile method in servie and controller

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,11 +1,12 @@
 import { UserEntity } from '@app/user/entites/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FollowEntity } from './follow.entity';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, FollowEntity])],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
