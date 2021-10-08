72d58f589c724f3f229ab250aa87686bedc6197c
Hamza Zahiul Islam <hamzazahidulislam444@gmail.com>
2021-10-08 22:21:42 +0300

    add unfollow method in controller and service

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,14 @@
 import { User } from '@app/user/decorators/user.decorator';
 import { AuthGuard } from '@app/user/guards/auth.guard';
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import {ProfileService} from './profile.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ProfileService } from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 
 @Controller('profiles')
@@ -19,7 +26,7 @@ export class ProfileController {
     );
     return this.profileService.buildProfileResponse(profile);
   }
- 
+
   @Post(':username/follow')
   @UseGuards(AuthGuard)
   async followProfile(
@@ -33,4 +40,16 @@ export class ProfileController {
     return this.profileService.buildProfileResponse(profile);
   }
 
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unfollowProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -64,6 +64,33 @@ export class ProfileService {
     return { ...user, following: true };
   }
 
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and Following cant be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
+  }
+
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
     delete profile.email;
     return { profile };
