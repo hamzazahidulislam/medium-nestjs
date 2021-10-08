ec8876f57ff15c6d4c185771f38df2b33a03f846
Hamza Zahiul Islam <hamzazahidulislam444@gmail.com>
2021-10-08 19:21:42 +0300

    create a get profile in contorller and service

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { User } from '@app/user/decorators/user.decorator';
+import { Controller, Get, Param } from '@nestjs/common';
+import {ProfileService} from './profile.service';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
 
-@Controller('profile')
-export class ProfileController {}
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
